f95d022df49c3c34d45bf413e43a7801422db494
nconlisk <noecon@gmail.com>
2023-09-21 17:47:54 +0100

    started work on modal for automatic book import

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 import Modal from "./Modal";
+import GetBook from "./BookImport";
 
 
 
@@ -8,6 +9,7 @@ const ListHeader = ({listName, getData}) => {
 
   const [cookies, setCookie, removeCookie] = useCookies(null)
   const [showModal, setShowModal] = useState(false)
+  const [showImport, setShowImport] = useState(false)
 
 
     const signOut = () => {
@@ -22,9 +24,11 @@ const ListHeader = ({listName, getData}) => {
       <div className="list-header">
         <h1>{listName}</h1>
         <div className="button-container">
+            <button className="create" onClick={() => setShowImport(true)}>IMPORT</button>
             <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
             <button className="signout" onClick={signOut}>SIGN OUT</button>
-        </div> 
+        </div>
+        {showImport && <GetBook mode={'create'} setShowImport={setShowImport} getData={getData}/>} 
         {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData}/>}         
       </div>
     );
