e9329a0199aaf87712c7cc2cdaf0a8afe30b8996
nconlisk <noecon@gmail.com>
2023-10-12 16:26:34 +0100

    modified db connection for vercel deployment

--- server/db.js
+++ server/db.js
@@ -1,16 +1,30 @@
 // setting up db for deploying
 
-const Pool = require('pg').Pool;    // from postgres docs
+const Pool = require('pg-pool');    // from postgres docs
+const url = require('url');
 
-require('dotenv').config()         // for using .env for secret info
+const params = url.parse(process.env.POSTGRES_URL);
+const auth = params.auth.split(':');
 
-const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.DBPORT,
-    database: process.env.POSTGRES_DATABASE,
+const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
     ssl: true
-})
+};
+
+const pool = new Pool(config);
+
+//require('dotenv').config()         // for using .env for secret info
+
+// const pool = new Pool({
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.DBPORT,
+//     database: process.env.POSTGRES_DATABASE
+// })
 
 module.exports = pool
